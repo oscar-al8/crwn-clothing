24e75f07be0670dfa0524d3c849890d93c6d6a08
Oscar Alvarado <oscarmehrer@hotmail.com>
2024-07-19 21:20:07 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Directory from './components/directory/Directory'
+import categories from './categories.json'
+
+const App = () => {
+	return <Directory categories={categories} />
+}
+
+export default App
